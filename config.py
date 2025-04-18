88c4257f9aef131651c5301ac6dac8e08d818275
Ragmataz <deepakupreti100us@gmail.com>
2025-04-18 11:18:54 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+# scanner/config.py
+
+import os
+
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+
+if not TELEGRAM_BOT_TOKEN or not TELEGRAM_CHAT_ID:
+    raise Exception("Telegram credentials not found. Check GitHub Secrets.")
