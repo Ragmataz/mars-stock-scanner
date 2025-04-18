10c97e8d87f68effa3202c8e1acc662ef903ad8f
Ragmataz <deepakupreti100us@gmail.com>
2025-04-18 19:41:46 +0530

    Update telegram_bot.py

--- scanner/telegram_bot.py
+++ scanner/telegram_bot.py
@@ -1,20 +1,18 @@
 import os
 import requests
 
-TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
-
-def send_telegram_message(text):
-    url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
-    payload = {
-        "chat_id": TELEGRAM_CHAT_ID,
-        "text": text
-    }
+def send_telegram_message(message):
+    token = os.getenv("TELEGRAM_BOT_TOKEN")
+    chat_id = os.getenv("TELEGRAM_CHAT_ID")
+    url = f"https://api.telegram.org/bot{token}/sendMessage"
+    payload = {"chat_id": chat_id, "text": message, "parse_mode": "HTML"}
     requests.post(url, data=payload)
 
-def send_telegram_message_with_image(caption, image_path):
-    url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendPhoto"
-    with open(image_path, 'rb') as image_file:
-        files = {'photo': image_file}
-        data = {"chat_id": TELEGRAM_CHAT_ID, "caption": caption}
+def send_telegram_message_with_image(message, image_path):
+    token = os.getenv("TELEGRAM_BOT_TOKEN")
+    chat_id = os.getenv("TELEGRAM_CHAT_ID")
+    url = f"https://api.telegram.org/bot{token}/sendPhoto"
+    with open(image_path, "rb") as image:
+        files = {"photo": image}
+        data = {"chat_id": chat_id, "caption": message, "parse_mode": "HTML"}
         requests.post(url, files=files, data=data)
