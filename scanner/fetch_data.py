671f77bb645b047a6f7444e6d22336d83d9f76f9
Ragmataz <deepakupreti100us@gmail.com>
2025-04-18 11:45:22 +0530

    Update fetch_data.py

--- scanner/fetch_data.py
+++ scanner/fetch_data.py
@@ -1,22 +1,20 @@
-# scanner/fetch_data.py
-
 import yfinance as yf
 import pandas as pd
 from datetime import datetime, timedelta
-
-def get_data(ticker, period="6mo", interval="1d"):
-    data = yf.download(ticker, period=period, interval=interval, progress=False)
-    data.dropna(inplace=True)
-    return data
+from scanner.config import NSE_500_TICKERS, INDEX_TICKER
 
 def get_nse500_list():
-    url = "https://archives.nseindia.com/content/indices/ind_nifty500list.csv"
-    return pd.read_csv(url)["Symbol"].tolist()
+    return NSE_500_TICKERS
 
-def get_index_symbol(index="CNX500"):
-    return {
-        "CNX500": "^CRSLDX",
-        "NIFTY": "^NSEI",
-        "MIDCAP150": "^CNXMIDCAP",
-        "SMALLCAP": "^CNXSMCP"
-    }.get(index.upper(), "^CRSLDX")
+def get_index_symbol():
+    return INDEX_TICKER
+
+def get_data(stock_list, index_symbol):
+    end = datetime.now()
+    start = end - timedelta(days=365)
+
+    all_data = {}
+    for symbol in stock_list + [index_symbol]:
+        df = yf.download(symbol + ".NS", start=start, end=end, progress=False)
+        all_data[symbol] = df
+    return all_data
